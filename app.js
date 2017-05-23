fb6e1c75873e41b41e2e2369bc8f6f6ee641a6d1
Sergey <sergey.riedel@gmail.com>
2017-05-23 14:47:11 +0400

    Set up reading input data

--- app.js
+++ app.js
@@ -1,35 +1,83 @@
-var budgetController = (function () {
+// BUDGET CONTROLLER
 
-    var x = 23;
+var budgetController = (function() {
 
-    var add = function (a) {
-      return x + a;
+  var x = 23;
+
+  var add = function(a) {
+    return x + a;
+  }
+
+  return {
+    publicTest: function(b) {
+      return add(b);
+    }
+  }
+})();
+
+
+// UI CONTROLLER
+
+var UIController = (function() {
+
+  var DOMStrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  };
+
+
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMStrings.inputType).value,
+        description: document.querySelector(DOMStrings.inputDescription).value,
+        value: document.querySelector(DOMStrings.inputValue).value
+      };
+    },
+
+    getDOMstrings: function() {
+      return DOMStrings;
     }
 
-    return {
-        publicTest: function (b) {
-            return add(b);
-        }
-    }
+  };
+
 })();
 
 
 
-var UIController = (function () {
+// GLOBAL CONTROLLER
 
-    // Some code
+var controller = (function(budgetCtrl, UICtrl) {
 
-})();
+  var DOM = UICtrl.getDOMstrings();
+
+  var ctrlAddItem = function() {
+
+    // 1. Get field input data
+    var input = UICtrl.getInput();
+    console.log(input);
+
+    // 2. Add the item to the budget controller
+
+    // 3. Add the item to the UI
+
+    // 4. Calculate the budget
+
+    // 5. Display the budget on the UI
 
 
-var controller = (function (budgetCtrl, UICtrl) {
+  }
 
-    var z = budgetCtrl.publicTest(5);
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
-    return {
-        anotherPublic: function () {
-            console.log(z);
-        }
+  document.addEventListener('keypress', function(e) {
+
+    if (e.keyCode === 13 || e.which === 13) {
+      ctrlAddItem();
     }
+
+  });
 
 })(budgetController, UIController);
