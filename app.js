eef6eae176b8887b95a54ad8ac8bd6ddba99ebac
Sergey <sergey.riedel@gmail.com>
2017-05-29 18:19:28 +0400

    Improve 'Add item' function

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ var UIController = (function() {
       return {
         type: document.querySelector(DOMStrings.inputType).value,
         description: document.querySelector(DOMStrings.inputDescription).value,
-        value: document.querySelector(DOMStrings.inputValue).value
+        value: parseFloat(document.querySelector(DOMStrings.inputValue).value)
       };
     },
 
@@ -150,6 +150,18 @@ var controller = (function(budgetCtrl, UICtrl) {
     });
   };
 
+  var updateBudget = function() {
+
+    // 1. Calculate the budget
+
+    // 2. Return the budget
+
+    // 3. Display the budget on the UI
+
+
+
+  };
+
 
   var ctrlAddItem = function() {
     var input, newItem;
@@ -157,19 +169,20 @@ var controller = (function(budgetCtrl, UICtrl) {
     // 1. Get field input data
     input = UICtrl.getInput();
 
-    // 2. Add the item to the budget controller
-    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+    if (input.description !== '' && !isNaN(input.value) && input.value > 0) {
+      // 2. Add the item to the budget controller
+      newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
 
-    // 3. Add the item to the UI
-    UICtrl.addListItem(newItem, input.type);
+      // 3. Add the item to the UI
+      UICtrl.addListItem(newItem, input.type);
 
-    // 4. Clear fields
-    UICtrl.clearFields();
+      // 4. Clear fields
+      UICtrl.clearFields();
 
-    // 4. Calculate the budget
-
-    // 5. Display the budget on the UI
+      // 5. Calculate and update budget
+      updateBudget();
+    }
 
   };
 
